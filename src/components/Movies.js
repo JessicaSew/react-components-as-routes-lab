576cd8216f43fa38bfd4fdc22e37b8ed6763d396
jessicasewell <sewell.jessica95@gmail.com>
2021-07-06 14:26:53 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,8 +4,10 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+       <div>{movies.title}</div>
     </div>
+    
   );
 };
 
